df3ad2887389010f84894fd1e4c4397e2d2f47dd
nayunis <mueller.jolanda@gmail.com>
2025-11-19 01:37:41 +0000

    added pytests for Transition and State classes

---
+++ tests/test_state.py
@@ -0,0 +1,24 @@
+# tests/test_state.py
+
+def test_import_state():
+    from MLJ.physics.state import State
+    assert hasattr(State, "__doc__")
+
+
+def test_default_state():
+    from MLJ.physics.state import State
+    
+    default_state = State()
+    assert default_state.energy == 0.0
+    assert default_state.name == "Ground State"
+    assert default_state.hW == 0.15
+
+def test_custom_state():
+    from MLJ.physics.state import State
+    
+    default_state = State(name="LE", energy=1.5, number_of_vibronic_modes=12,hW=0.2,sigma=0.1)
+    assert default_state.energy == 1.5
+    assert default_state.name == "LE"
+    assert default_state.number_of_vibronic_modes == 12
+    assert default_state.hW == 0.2
+    assert default_state.sigma == 0.1
---
+++ tests/test_transition.py
@@ -0,0 +1,47 @@
+# tests/test_transition.py
+
+def test_import_transition():
+    from MLJ.physics.transition import Transition
+    from MLJ.physics.transition import TransitionType
+    assert hasattr(Transition, "__doc__")
+    assert hasattr(TransitionType, "__doc__")
+
+def test_transition_energy():
+    from MLJ.physics.state import State
+    from MLJ.physics.transition import Transition
+
+    ground_state = State()
+    excited_state = State("LE",energy=1.5)
+    
+    transition1 = Transition(state_low_energy=ground_state, state_high_energy=excited_state)
+    transition2 = Transition(excited_state, ground_state)
+    transition3 = Transition(excited_state)
+    transition4 = Transition(ground_state, excited_state)
+
+    energy_difference = abs(excited_state.energy - ground_state.energy)
+    assert energy_difference == transition1.energy_difference
+    assert energy_difference == transition2.energy_difference
+    assert energy_difference == transition3.energy_difference
+    assert energy_difference == transition4.energy_difference
+
+def test_transition_type():
+    from MLJ.physics.state import State
+    from MLJ.physics.transition import Transition
+    from MLJ.physics.transition import TransitionType
+
+    excited_state = State("LE",energy=1.5)
+    transition = Transition(excited_state)
+
+    assert transition.transition_type == TransitionType.RECOMBINATION
+
+
+def test_huang_rhys():
+    from MLJ.physics.state import State
+    from MLJ.physics.transition import Transition
+
+    ground_state = State()
+    excited_state = State("LE",energy=1.5)
+    
+    transition = Transition(ground_state, excited_state)
+    huang_rhys = transition.lambda_inner / excited_state.hW
+    assert huang_rhys == transition.huang_rhys
