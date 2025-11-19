c229911bb741eb1b0bc0cabb4abe364c054eb759
tim-rein-qd <tim.rein@quantumdiamonds.de>
2025-11-19 13:18:46 +0000

    Improve variable-naming of the constants and in the FCWD calculations

--- src/MLJ/physics/constants.py
+++ src/MLJ/physics/constants.py
@@ -9,14 +9,13 @@
 #####################################################################################
 
 """ Physical Constants """
-q  = 1.602176565e-19        # Elementary Charge (C)
-c  = 299792458              # Speed of Light (m/s)
+CONST_q  = 1.602176565e-19        # Elementary Charge (C)
+CONST_c  = 299792458              # Speed of Light (m/s)
 
-h  = 6.62606957e-34         # Planck constant (J*s)
-hbar = 1.054571817e-34      # Reduced Planck constant (J*s)
+CONST_h  = 6.62606957e-34         # Planck constant (J*s)
+CONST_hbar = 1.054571817e-34      # Reduced Planck constant (J*s)
 
-kB_J = 1.380649e-23         # Boltzmann constant in J/K
-kB_eV = 8.617333262e-5      # Boltzmann constant in eV/K
+CONST_kB_J = 1.380649e-23         # Boltzmann constant in J/K
+CONST_kB_eV = 8.617333262e-5      # Boltzmann constant in eV/K
 
-me = 9.1093837015e-31       # Electron mass (kg)
-
+CONST_me = 9.1
