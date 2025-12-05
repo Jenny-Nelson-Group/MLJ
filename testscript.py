f3401965e99919b03b9f9bd3a75512fff5339e86
JolandaSMuller <j.muller21@ic.ac.uk>
2025-12-05 17:13:21 +0000

    random changes

--- testscript.py
+++ testscript.py
@@ -4,20 +4,29 @@ from MLJ.physics.transition import Transition, TransitionType
 import numpy as np
 import matplotlib.pyplot as plt
 
-GS = State()
-LE = State(name="Local Exciton", index=1, energy=1.5)
+GS = State(number_of_vibronic_modes=15)
+LE = State(name="Local Exciton", index=1, energy=1.5, number_of_vibronic_modes=5)
 
-exc =  Transition(state_low_energy=GS, state_high_energy=LE, transition_type=TransitionType.ABSORPTION)
+abs =  Transition(state_low_energy=GS, state_high_energy=LE, transition_type=TransitionType.ABSORPTION, lambda_inner=0.8, lambda_outer=0.01)
+rec =  Transition(state_low_energy=GS, state_high_energy=LE, transition_type=TransitionType.RECOMBINATION, lambda_inner=0.8, lambda_outer=0.01)
 
-print(exc)
+print(abs)
+print(rec)
 
 res=200
 energies = np.linspace(0.8, 1.8, res)
-fcwd_values = fcwd(energies, exc)
+fcwd_values_abs = fcwd(energies, abs)
+fcwd_values_rec = fcwd(energies, rec)
 
 x= energies
-y=fcwd_values
-y2 = y.reshape(res, 2)
-plt.plot(x, y2[:, 0])
-plt.plot(x, y2[:, 1])
+y_abs=fcwd_values_abs
+y_abs = y_abs.reshape(res, 2)
+
+y_rec=fcwd_values_rec
+y_rec = y_rec.reshape(res, 2)
+
+plt.plot(x, y_abs[:, 0])
+#plt.plot(x, y_abs[:, 1])
+plt.plot(x, y_rec[:, 0])
+#plt.plot(x, y_rec[:, 1])
 plt.show()
