fixing normalsiation to work vecotrised and allowing for custom disorder distributions - added basics to store basic functions, moved those functions out of normalisation - many fixes in "normalisation" so that dimensions now work for vectorised calculations - assigned disorder_distribution as a callable parameter of State, so that down the line the user of the package could also define custom functions (without having to edit the package itself) - added docstring in transition, and updated the __repr__ to show the transition type - added a property for transition that returns the disorder distribution directly from the transition for convenience - removed unused imports - updated testsscripts to check that partition function returns values of the right dimensions

src/MLJ/physics/normalisation.py
@@ -11,22 +11,27 @@
 
 from MLJ.physics.transition import TransitionType
 from MLJ.physics.transition import Transition
-import MLJ.physics.constants as const
+from MLJ.physics.basics import boltzmann
 import numpy as np
 from typing import Sequence
 
+
 def partition_function(transition: Transition) -> Sequence[float]:
+    """Return the normalisation factor from the integrated partition function for each temperature."""
 
-    temperature = np.array([300.0,200.0])
+    temperatures = np.array([300.0,200.0])
 
-    disorder_energies = disorder_function(transition.gibbs_energies, transition.mean_gibbs_energy, transition.state_high_energy.disorder_sigma)
-    boltzmann_factor =  boltzmann(transition.gibbs_energies, temperature) if (transition.transition_type == TransitionType.RECOMBINATION) else 1
-    partition_function = np.trapezoid(transition.gibbs_energies, disorder_energies * boltzmann_factor)
+    disorder_energies = transition.disorder_distribution(
+                x = transition.gibbs_energies,
+                mean = transition.mean_gibbs_energy,
+                sigma = transition.state_high_energy.disorder_sigma
+                )
+    if (transition.transition_type == TransitionType.RECOMBINATION):
+        boltzmann_factor = boltzmann(transition.gibbs_energies, temperatures)
+    else:
+        boltzmann_factor = 1
+
+    integrand = disorder_energies[:, None] * boltzmann_factor
+    partition_function = np.trapezoid(integrand, x=transition.gibbs_energies, axis=0)
 
     return partition_function
-
-def boltzmann(energy, temperature):
-    return np.exp(-energy/(temperature*const.BOLTZMANN_CONSTANT_EV))
-
-def disorder_function(energy, mean_energy, sigma):
-    return np.exp(-0.5*(energy-mean_energy)^2/sigma^2)

src/MLJ/physics/state.py
@@ -8,28 +8,39 @@
 # Date: November 2025
 #####################################################################################
 
+from MLJ.physics.basics import gaussian
+from typing import Callable
+import numpy as np
+
+DistributionFunction = Callable[[np.ndarray, float, float], np.ndarray]
+
 class State:
     """Represents a quantum state with its properties."""
     def __init__(
             self,
             index: int = 0,
             name: str = "Ground State",
+            energy: float = 0.0,
             number_of_vibronic_modes: int = 15,
             vib_spacing: float = 0.1500,
             disorder_sigma: float = 0.05,
             disorder_number_of_states: float = 21,
             disorder_integration_cut_off: float = 2.5,
-            energy: float = 0.0
+            disorder_distribution: DistributionFunction | None = None,
         ) -> None:
 
         self.index: int = index
         self.name: str = name
+        self.energy: float = energy
         self.number_of_vibronic_modes: int = number_of_vibronic_modes
         self.vib_spacing: float = vib_spacing
         self.disorder_sigma: float = disorder_sigma
         self.disorder_number_of_states: float = disorder_number_of_states
         self.disorder_integration_cut_off: float = disorder_integration_cut_off
-        self.energy: float = energy
+
+        self.disorder_distribution: DistributionFunction = (
+            gaussian if disorder_distribution is None else disorder_distribution
+        )
 
     def __repr__(self) -> str:
         return f"State(Name='{self.name}', Energy={self.energy:.4f} eV)"

src/MLJ/physics/transition.py
@@ -8,7 +8,6 @@
 # Date: November 2025
 #####################################################################################
 
-from typing import Callable, Any, Optional
 from MLJ.physics.state import State
 from enum import Enum
 import numpy as np
@@ -53,19 +52,26 @@ class Transition:
             self.state_high_energy: State = state_low_energy
 
     def set_gibbs_energies(self):
-        """"""
+        """Calculate the spread of energies based on the disorder."""
         self.mean_gibbs_energy: float = self.state_high_energy.energy - self.state_low_energy.energy
 
         cut_off = self.state_high_energy.disorder_integration_cut_off * self.state_high_energy.disorder_sigma
         self.gibbs_energies = np.linspace(self.mean_gibbs_energy - cut_off, self.mean_gibbs_energy + cut_off, self.state_high_energy.disorder_number_of_states)
 
     def set_type_absorption(self):
+        """Set the transition type to Absorption."""
         self.transition_type = TransitionType.ABSORPTION
         return self
 
     def set_type_recombination(self):
+        """Set the transition type to Recombination."""
         self.transition_type = TransitionType.RECOMBINATION
         return self
+
+    @property
+    def disorder_distribution(self):
+        """Return the disorder distribution of the high energy state."""
+        return self.state_high_energy.disorder_distribution
 
     @property
     def huang_rhys(self):
@@ -75,7 +81,6 @@ class Transition:
         else:
             raise ValueError("state_high_energy.vib_spacing must not be zero when computing Huang-Rhys.")
 
-
     def __repr__(self) -> str:
         return (f"Transition(Low-energy state ='{self.state_low_energy.name}', High-energy state='{self.state_high_energy.name}', "
-                f"Energy Difference={self.mean_gibbs_energy:.4f} eV, Huang Rhys Factor={self.huang_rhys:.4f})")
+                f"Energy Difference={self.mean_gibbs_energy:.4f} eV, Huang Rhys Factor={self.huang_rhys:.4f}), Type={self.transition_type}")

testscript.py
@@ -1,4 +1,5 @@
 from MLJ.physics.FCWD import fcwd
+from MLJ.physics.normalisation import partition_function
 from MLJ.physics.state import State
 from MLJ.physics.transition import Transition, TransitionType
 import numpy as np
@@ -7,16 +8,17 @@ import matplotlib.pyplot as plt
 GS = State(number_of_vibronic_modes=15)
 LE = State(name="Local Exciton", index=1, energy=1.5, number_of_vibronic_modes=5)
 
-abs =  Transition(state_low_energy=GS, state_high_energy=LE, transition_type=TransitionType.ABSORPTION, lambda_inner=0.8, lambda_outer=0.01)
-rec =  Transition(state_low_energy=GS, state_high_energy=LE, transition_type=TransitionType.RECOMBINATION, lambda_inner=0.8, lambda_outer=0.01)
+transition =  Transition(state_low_energy=GS, state_high_energy=LE, lambda_inner=0.8, lambda_outer=0.01)
+print(transition)
 
-print(abs)
-print(rec)
+Zrec = partition_function(transition)
+
+print(Zrec)
 
 res=200
 energies = np.linspace(0.8, 1.8, res)
-fcwd_values_abs = fcwd(energies, abs)
-fcwd_values_rec = fcwd(energies, rec)
+fcwd_values_abs = fcwd(energies, transition.set_type_absorption())
+fcwd_values_rec = fcwd(energies, transition.set_type_recombination())
 
 x= energies
 y_abs = fcwd_values_abs[:,0,0]