b073b012355b65925110b973cbd4806fac9316a8
nayunis <mueller.jolanda@gmail.com>
2025-12-10 22:19:27 +0000

    some additions: - added normalised and non-normalised version of gaussian, - explicitly treat the sigma=0 case - added an integral function, where we can define in one place, how integrals are done everywhere. - at the moment this uses trapezoid normally, but for the edge case where there is just one value energy it simply returns the value.

--- src/MLJ/physics/basics.py
+++ src/MLJ/physics/basics.py
@@ -2,10 +2,26 @@ import numpy as np
 import MLJ.physics.constants as const
 
 def gaussian(x, mean, sigma):
-    return np.exp(-0.5 * ((x - mean) / sigma)**2)
+    """Returns non-normalised Gaussian or 1 if sigma is 0."""
+    if sigma == 0:
+        return np.array([1])
+    else:
+        gaussian_weight =  np.exp(-0.5 * ((x - mean) / sigma)**2)
+        return gaussian_weight
+
+def gaussian_norm(x, mean, sigma):
+    if sigma == 0:
+        return np.array([1.0])
+    return (1.0 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mean) / sigma)**2)
 
 
 def boltzmann(energy, temperature):
     return np.exp(
         -energy[:, None] / (temperature[None, :] * const.BOLTZMANN_CONSTANT_EV)
     )
+
+def integral(y, x):
+    if y.shape[0] == 1:
+        return y[0]
+    
+    return np.trapezoid(y, x=x, axis=0)
