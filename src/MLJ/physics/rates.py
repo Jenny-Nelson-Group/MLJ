87c7d90749faa07aa905edac019ca46477b5a757
nayunis <mueller.jolanda@gmail.com>
2025-12-20 07:17:15 +0000

    clean up docstrings in rates

--- src/MLJ/physics/rates.py
+++ src/MLJ/physics/rates.py
@@ -29,7 +29,7 @@ class Rates:
     Parameters
     ----------
     transition : Transition
-        Object containing states, coupling strengths, and disorder parameters.
+        Containing states, reorganisation energies, coupling strengths, and disorder parameters.
     photon_energies : np.ndarray
         1D array of photon energies [eV] at which to evaluate spectral rates.
     temperatures : np.ndarray, optional
@@ -70,7 +70,7 @@ class Rates:
         """Total non-radiative rate"""
         knrad = self.rate_calculation(process = ProcessType.RECOMBINATION, is_non_radiative=True)
         return knrad.squeeze(axis=0)
-        
+
     @cached_property
     def rate_recombination_total(self):
         """The total recombination rate (Radiative + Non-Radiative)."""
@@ -78,30 +78,30 @@ class Rates:
 
     @cached_property
     def boltzmann_factor(self):
-        """ Broadcasting the grid and temperatures into a 3D tensor. Shape: [1, N_disorder, N_temperatures] """
+        """ Broadcasting the grid and temperatures into a 3D tensor (shape: (1, N_disorder_energies, N_temperatures))"""
         grid = self.transition.gibbs_energy_grid
         return boltzmann(grid[None, :, None], self.temperatures[None, None, :])
-    
+
     @cached_property
     def norm_recombination(self):
-        """Partition function result cached for the current temperatures."""
+        """Partition function (shape: (N_disorder_energies, N_temperatures)) result cached for the current temperatures."""
         return partition_function(self.transition, self.temperatures, ProcessType.RECOMBINATION)
 
     @cached_property
     def norm_absorption(self):
-        """Partition function result cached for the current temperatures."""
+        """Partition function (shape: (N_disorder_energies, N_temperatures)) result cached for the current temperatures."""
         return partition_function(self.transition, self.temperatures, ProcessType.ABSORPTION)
 
     @cached_property
     def energy_prefactor(self):
-        """Energy prefactor for radiative transitions."""
+        """Energy prefactor for radiative transitions with (shape(photon_energies))"""
         return (self.photon_energies/const.SPEED_OF_LIGHT)**3
-    
+
 
     def rate_calculation(self, process, is_non_radiative):
         """
         Core physics engine for calculating the rates.
-        Radiative (absorption and recombination) and non-radiative rates are 
+        Radiative (absorption and recombination) and non-radiative rates are
         handeled together by this function.
 
         Parameters
@@ -117,28 +117,30 @@ class Rates:
             A 2D array of transition rates.
             - Shape: (N_photon_energies, N_temperatures)
             - For non-radiative rates: The energy axis has length 1 (at 0 eV).
-            - Units: check... 
+            - Units: check...
         """
-        transition = self. transition
+        transition = self.transition
 
         # 1. Setup Process-Specific Parameters
         if is_non_radiative:
             calculation_energies = np.array([0.0])
             energy_term = np.array([1.0])
-            prefactor = _prefactor_nrad            
+            prefactor = _prefactor_nrad
             coupling = transition.coupling_non_radiative
         else:
             calculation_energies = self.photon_energies
             energy_term = self.energy_prefactor
             prefactor = _prefactor_rad
             coupling = transition.coupling_radiative
-        
+
         # 2. Get FCWD
         fcwd_ = fcwd.fcwd(photon_energies=calculation_energies, transition=transition,
                           temperatures=self.temperatures, process=process)
 
-        # 3. Apply Boltzmann to recombination processes and select partition function
+        # 3. Determine the integrand
         integrand = fcwd_ * transition.disorder_weights[None, :, None]
+
+        # apply Boltzmann to recombination processes and select partition function
         match process:
             case ProcessType.RECOMBINATION:
                 integrand *= self.boltzmann_factor
@@ -152,8 +154,12 @@ class Rates:
         disorder_integral = integral(y = integrand, x = transition.gibbs_energy_grid, axis=1)
 
         # Get Rate
-        rate = prefactor * coupling**2 * energy_term[:,None]  * disorder_integral / norm[None,:]
+        rate = (prefactor # shape(1)
+                * coupling**2 # shape(1)
+                * energy_term[:,None] # shape(photon_energies,1)
+                * disorder_integral # shape(photon_energies,temperatures)
+                / norm[None,:]) # shape(1,temperatures)
         if process==ProcessType.ABSORPTION:
             rate *= self.photon_density
-        
-        return rate 
+
+        return rate
