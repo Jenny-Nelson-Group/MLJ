246fc4570ba36f3edf5d81cc2eb867f6fa0645a6
nayunis <mueller.jolanda@gmail.com>
2026-02-07 07:52:55 +0000

    simple plotting with nicer colours

--- src/MLJ/__init__.py
+++ src/MLJ/__init__.py
@@ -16,8 +16,9 @@ from MLJ.physics.state import State
 from MLJ.physics.transition import Transition
 from MLJ.physics.rates import Rates
 from MLJ.physics.simulate_system import StateSystem
+from MLJ.helpers.plotting import plot_PL
 
-__all__ = ["State", "Transition", "Rates", "StateSystem"]
+__all__ = ["State", "Transition", "Rates", "StateSystem", "plot_PL"]
 
 
 def run(*, example=False, **kwargs):
--- test_scripts/pl_examples.py
+++ test_scripts/pl_examples.py
@@ -1,22 +1,27 @@
 import MLJ as mlj
+from MLJ.helpers.plotting import plot_PL
+from MLJ.physics.config import config
 import matplotlib.pyplot as plt
 import numpy as np
-from MLJ.physics.config import config
 
-config.temperatures_K = np.array([100, 300])
+n_temps = 10
+config.temperatures_K = np.linspace(50, 350, n_temps)
+config.photon_energies = np.linspace(0.8, 2.0, 300)
+k_LECT = np.ones(n_temps)
 gs = mlj.State(name="S0", energy=0.0)
-le = mlj.State(name="LE", energy=1.5)
-ct = mlj.State(name="CT", energy=1.3)
-trans_LE = mlj.Transition(le, gs)
-trans_CT = mlj.Transition(ct, gs)
-trans_LECT = mlj.Transition(le, ct, k_transfer=[10e5, 10e10])
-system = mlj.StateSystem([trans_LE, trans_CT, trans_LECT])
+le = mlj.State(name="LE", energy=1.5, disorder_sigma=0.02)
+ct = mlj.State(name="CT", energy=1.35, disorder_sigma=0.02)
+trans_LE = mlj.Transition(le, gs, lambda_outer=0.05)
+trans_CT = mlj.Transition(ct, gs, oscillator_strength=3)
+trans_LECT = mlj.Transition(le, ct, k_transfer=k_LECT)
+system_1 = mlj.StateSystem([trans_LE])
+system_2 = mlj.StateSystem([trans_LE, trans_CT, trans_LECT])
 
-print(system.transfers)
+print(system_2.transfers)
 print(trans_CT.name)
 print(trans_LE.name)
 print(trans_LECT.name)
 
-plt.plot(system.photon_energies, system.emission_photoluminescence)
-plt.plot(system.photon_energies, system.absorption)
+# plot_PL(system_1)
+plot_PL(system_2)
 plt.show()
