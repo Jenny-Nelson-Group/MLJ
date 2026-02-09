f298589f250168174b402d3cc79bc04aa70f8f49
JolandaSMuller <j.muller21@ic.ac.uk>
2026-02-09 19:39:56 +0000

    - added a jupyter notebook - made photon_energies optional in rates

--- src/MLJ/__init__.py
+++ src/MLJ/__init__.py
@@ -17,8 +17,9 @@ from MLJ.physics.transition import Transition
 from MLJ.physics.rates import Rates
 from MLJ.physics.simulate_system import StateSystem
 from MLJ.helpers.plotting import plot_PL
+from MLJ.physics.config import config
 
-__all__ = ["State", "Transition", "Rates", "StateSystem", "plot_PL"]
+__all__ = ["State", "Transition", "Rates", "StateSystem", "plot_PL", "config"]
 
 
 def run(*, example=False, **kwargs):
